2a7584539ed5e4b20e0f17f7edb0466d55e0ae57
Apo45ty <antonio.tapia@upr.edu>
2014-03-05 22:38:54 -0400

    first commit

---
+++ Assets/LeftTredmill.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftTredmill : MonoBehaviour {
+	public float dx = 0.5f;
+	public float dy = 0.5f;
+	void OnTriggerEnter2D(Collider2D collisionObject) {
+
+		collisionObject.gameObject.transform.Translate (Vector3.left * dx*Time.deltaTime);
+		collisionObject.gameObject.transform.Translate (Vector3.up * dy*Time.deltaTime);
+		collisionObject.gameObject.transform.Rotate (Vector3.up * Time.deltaTime, Space.World);
+		}
+}
---
+++ Assets/MovementClouds.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+public var speed:float = 1;
+	function Update()
+	{
+		if(transform.position.x > 38){
+			transform.position = new Vector2(-38,5.467466);
+		}
+		transform.Translate(Vector3.right*speed);
+	} 
