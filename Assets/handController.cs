946069a3ca6c81fc89a0db006190738519da3dbc
Apo45ty <antonio.tapia@upr.edu>
2014-03-24 19:10:27 -0400

    Added the kinect stuff

---
+++ Assets/handController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class kinnect : MonoBehaviour {
+	KUInterface kinect;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/kinectHandController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class kinectHandController : MonoBehaviour {
+
+	public KUInterface kinect;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(kinect != null){
+
+		}
+	}
+}
